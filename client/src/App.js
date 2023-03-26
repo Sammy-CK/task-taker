ccd91c7cbb01adaad45cae2fb31ef10bc756626d
Sammy-CK <sammycherono2696@gmail.com>
2023-03-26 04:28:07 +0300

    Added login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,33 @@
 import './App.css';
-import React from 'react';
+import {React, useState} from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Homepage from './components/homepage'
+import LogIn from "./components/login";
 
 
 
 function App() {
+
+  let [loginDetails, setLoginDetails] = useState({ username: "", password: "" });
+  let [user, setUser] = useState(null);
+
   return (
     <div className="App">
       <BrowserRouter>
       <Routes>
       <Route path='/' element={<Homepage />}/>
-
+      
+      <Route
+            path="/login"
+            element={
+              <LogIn
+                loginDetails={loginDetails}
+                setLoginDetails={setLoginDetails}
+                user={user}
+                setUser={setUser}
+              />
+            }
+          />
 
       </Routes>
       
---
+++ client/src/components/login.js
@@ -0,0 +1,82 @@
+import React from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import './style/login.css'
+
+function LogIn({ loginDetails, setLoginDetails, user, setUser }) {
+
+    let takeToTask = useNavigate();
+    return (
+      <div className="formDiv">
+  
+  <h1
+          style={{
+            textAlign: "left",
+            padding: "0px 10px 10px 10px",
+            fontSize: "4vw",
+            fontFamily: "fantasy",
+            width: "40%",
+          }}
+        >
+          TASK MANAGER
+        </h1>
+  
+        <div className="loginDiv">
+        <form
+          className="logInForm"
+          onSubmit={(e) => {
+            e.preventDefault();
+            // console.log(loginDetails)
+
+            fetch('/users/login', {
+                method: 'POST',
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(loginDetails)
+            })
+                .then(resp => {
+                    if(resp.ok){
+                        resp.json().then((user) => console.log(user))
+                    }else{
+                        resp.json().then((user) => console.log(user))
+                    }
+            })
+            
+            
+
+          }}
+        >
+          <h1>Log In</h1>
+          <label htmlFor="userName" style={{fontSize: "13px"}}>USERNAME</label>
+          <br />
+          <input
+            type="text"
+            id="userName"
+            required
+            onChange={(e) =>
+              setLoginDetails({ ...loginDetails, username: e.target.value })
+            }
+            value={loginDetails.name}
+          />{" "}
+          <br />
+          <label htmlFor="password">PASSWORD</label>
+          <br />
+          <input
+            type="password"
+            id="password"
+            required
+            onChange={(e) =>
+              setLoginDetails({ ...loginDetails, password: e.target.value })
+            }
+            value={loginDetails.password}
+          />
+          <br />
+          <input type="submit" id="submitBtn" value="LOGIN" />
+          <p>
+            Dont have an account? <NavLink to="/signup">SIGNUP</NavLink>
+          </p>
+        </form>
+        </div>
+      </div>
+    );
+}
+
+export default LogIn;
